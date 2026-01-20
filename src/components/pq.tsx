672087c8240f702866fb4d0b20a5ef8dee8901c5
Sahil Sawal <sahilsawal99@gmail.com>
2026-01-21 03:05:37 +0530

    done

--- src/components/pq.tsx
+++ src/components/pq.tsx
@@ -7,7 +7,7 @@ const PriorityQueue = ({
 }) => {
     return (
         <div className="w-[200px] mx-8">
-            <h1 className="mb-2 text-gray-700 dark:text-gray-300 text-[15px] text-center font-bold">Priority Queue</h1>
+            <h1 className="mb-2 text-gray-750 dark:text-gray-300 text-[15px] text-center font-bold">Priority Queue</h1>
             <table ref={pqRef} className="w-full">
                 <tbody>
                     <tr className="h-[30px] text-[14px]">
