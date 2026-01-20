6317d51ba173ac6c7bf4c89202abd6ff5b07be2a
Sahil Sawal <sahilsawal99@gmail.com>
2026-01-21 04:08:55 +0530

    done

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -7,13 +7,13 @@ const Slider = ({
 }) => {
     return (
         <div className="mx-8 mt-4 select-none">
-            <h1 className="text-gray-300 dark:text-gray-300 text-[12px] font-bold text-black dark:text-gray-300">Speed</h1>
-            <div ref={refs.sliderRef} className="w-[180px] h-2 bg-gray-300 dark:bg-gray-300 rounded inline-block hover:cursor-pointer">
-                <div ref={refs.thumbRef} className="-top-1 -ml-[6px] left-[50%] w-3 h-4 rounded-xl relative bg-slate-300 dark:bg-slate-400"></div>
+            <h1 className="text-[12px] font-bold text-black dark:text-gray-300">Speed</h1>
+            <div ref={refs.sliderRef} className="w-[180px] h-2 bg-black dark:bg-gray-300 rounded inline-block hover:cursor-pointer">
+                <div ref={refs.thumbRef} className="-top-1 -ml-[6px] left-[50%] w-3 h-4 rounded-xl relative bg-black dark:bg-slate-400"></div>
             </div>
             <table className="w-[180px]">
                 <tbody>
-                <tr className="text-gray-300 dark:text-gray-300 text-[11px] text-black dark:text-gray-300">
+                <tr className="text-[11px] text-black dark:text-gray-300">
                     <td className="w-[24%]">.25x</td>
                     <td className="w-[23%]">.5x</td>
                     <td className="w-[22%]">1x</td>
